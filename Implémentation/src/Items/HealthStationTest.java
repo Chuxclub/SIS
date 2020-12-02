0321dc0de48f3cab2d5a65e21ca4c797f9863675
alexis <seulementdjmortal@gmail.com>
2020-12-02 16:57:16 +0100

    bon nika sa race

---
+++ Implémentation/src/Items/HealthStationTest.java
@@ -0,0 +1,18 @@
+package Items;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HealthStationTest {
+
+    @org.junit.jupiter.api.Test
+    void describe() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isUsed() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testIsUsed() {
+    }
+}
