6383c2e06e84d1976dadeb106cfe80fdcb6ab62c
Florian <florian-1992@hotmail.fr>
2020-12-02 14:26:26 +0100

    Màj

--- Implémentation/src/Characters/Player.java
+++ Implémentation/src/Characters/Player.java
@@ -2,21 +2,31 @@ package Characters;
 
 import Location.Room;
 
+import java.util.Scanner;
+
 public class Player extends Actor {
 
 	private static String NAME = "player";
+	private Room previous_room;
 
 	public Player(Room r)
 	{
 		super(NAME, r);
+		this.previous_room = r;
 	}
 
 	public void back()
 	{
+		this.changeRoom(this.previous_room);
 	}
 
 	public void call()
 	{
+		System.out.print("Command :> ");
+		Scanner sc = new Scanner(System.in);
+
+		String verb = sc.next();
+
 	}
 
 	public void go(int Door)
