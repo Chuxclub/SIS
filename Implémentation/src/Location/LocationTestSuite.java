e25ef47bf0b01b4a3eb69980584b5cd0a7681de4
Florian <florian-1992@hotmail.fr>
2020-12-03 12:26:53 +0100

    Les tests sont prêts à être définis

--- Implémentation/src/Location/LocationTestSuite.java
+++ Implémentation/src/Location/LocationTestSuite.java
@@ -4,7 +4,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({Doors.DoorIT.class})
+@Suite.SuiteClasses({})
 
 public class LocationTestSuite {
 }
